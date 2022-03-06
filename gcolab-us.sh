db5d12c1879cb81a2f5e2fd3a46d7ee3adb91bff
zack-mining <99808194+zack-mining@users.noreply.github.com>
2022-03-06 20:12:07 -0300

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o us-west.minexmr.com:443 -u 88BRRbPa85QU6dmQH3e7HyFg6NSPQvEGyv7JeL5xbvP4ZeKQAP6AWZV9MWXAUd3SBQodqeHhh6cVRR9WuktaxcZnn8w2wX3S -k --tls --rig-id GC-US
+./xmrig -o us-west.minexmr.com:443 -u 8BRRbPa85QU6dmQH3e7HyFg6NSPQvEGyv7JeL5xbvP4ZeKQAP6AWZV9MWXAUd3SBQodqeHhh6cVRR9WuktaxcZnn8w2wX3S -k --tls --rig-id GC-US
